c37003e59a6d57c60ce42649a3081080cbea166d
RandomSavage <50834695+RandomSavage@users.noreply.github.com>
2021-09-16 01:40:07 -0400

    Added Route from 1st DEmo To Hello Controller

--- springDemo2/src/main/java/com/example/springboot/HelloController.java
+++ springDemo2/src/main/java/com/example/springboot/HelloController.java
@@ -1,7 +1,8 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
@@ -10,4 +11,11 @@ public class HelloController {
   public String index() {
     return "Greetings from Spring Boot!";
   }
+
+  @GetMapping("/hello")
+	public String hello(@RequestParam(value="name", defaultValue="world") String name) {
+		return String.format("Hello %s!", name);
+		// mvnw spring-boot:run 
+		// http://localhost:8080/hello?name=billy
+	}
 }
